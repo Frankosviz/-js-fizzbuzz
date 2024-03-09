aba178ac00e1d1dc38acbbc806c4d67c1f632759
Frankosviz <geom.francescopaolo@gmail.com>
2024-03-09 01:14:33 +0100

    assigned variables

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+    let numeri1a100 = 1;
+
+    for (let numeri1a100 = 1; numeri1a100 <= 100; numeri1a100++) {
+        console.log(numeri1a100);
+    }
+    
+
+    let multipli3 = 3;
+
+    for (let multipli3 = 3; multipli3 <= 99; multipli3 += 3) {
+        console.log(multipli3);
+    }
+    let multipli5 = 5;
+
+    for (let multipli5 = 5; multipli5 <= 95; multipli5 += 5) {
+        console.log(multipli5);
+    }
+    let multipli3e5 = 15;
+
+    for (let multipli3e5 = 15; multipli3e5 <= 75; multipli3e5 += 15) {
+        console.log(multipli3e5);
+    }
+
